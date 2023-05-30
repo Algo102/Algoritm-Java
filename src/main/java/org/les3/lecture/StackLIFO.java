e93cd31ee5eced7dc1d25f36155042401788c86e
Algo <algor102@gmail.comm>
2023-05-30 09:01:11 +0500

    обавил лекцию2 - Связаные списки и очередь

---
+++ src/main/java/org/les3/lecture/StackLIFO.java
@@ -0,0 +1,46 @@
+package org.les3.lecture;
+// Частный случай связного списка
+// Стек или LIFO, когда очередь уменьшается с конца, принцип стопки бумаг
+public class StackLIFO {
+    Node head;
+
+    // Положить данные. В начало очереди
+    public void push(int value){
+        Node node = new Node();
+        node.value = value;
+        node.next = head;
+        head = node;
+    }
+
+    // Извлечение данных, ничего не принимает, так как извлекает
+    public Integer pop(){
+        Integer result = null;
+        if (head != null){
+            result = head.value;
+            head = head.next;
+        }
+        return result;
+    }
+
+    public void revert(){
+        if(head != null && head.next != null) {
+            Node temp = head;
+            revert(head.next, head);
+            temp.next = null;
+        }
+    }
+
+    public void revert(Node currentNode, Node previousNode){
+
+        if(currentNode.next == null)
+            head = currentNode;
+        else
+            revert(currentNode.next, currentNode);
+        currentNode.next = previousNode;
+    }
+
+    public  class Node{
+        int value;
+        Node next;
+    }
+}
